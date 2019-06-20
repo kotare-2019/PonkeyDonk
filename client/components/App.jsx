52f08992de5dfcc09841d94e604f3a8a08ed8b5e
mikeywipo <49007478+mikeywipo@users.noreply.github.com>
2019-06-20 17:25:06 +1200

    update page displaying background and table

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Table from './Table'
+import Background from './Background'
+
 
 const App = () => {
   return (
+    <div>
     <h1> PonkeyDonk</h1>
+    <Background />
+    <Table />
+    </div>
   )
 }
 
--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1> PonkeyDonk</h1>
+    <h1> Table</h1>
   )
 }
 
