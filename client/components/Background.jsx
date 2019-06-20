c27a9e5496c170dc169942241bced081e8cb14a4
omphalus-kua <17259982+haluskua@users.noreply.github.com>
2019-06-20 18:32:55 +1200

    intial commit

--- client/components/Background.jsx
+++ client/components/Background.jsx
@@ -1,9 +1,75 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1> Background</h1>
-  )
+// Why do we place this outside the component?
+
+// const randomHexColor = () =>
+// `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+
+class Pixel extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      style: {
+        height: "300px",
+        width: "300px",
+        backgroundColor: this.randomHexColor()
+      }
+    }
+    // this.clickHandler = this.clickHandler.bind(this)
+    this.mouseOver = this.mouseOver.bind(this)
+  }
+
+  randomHexColor = () => {
+    return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  }
+
+  // colorShift = () => {
+  //     setInterval( () => {this.setState.backgroundColor = this.randomHexColor()}, 500 )
+  // }
+
+  // setInterval ( doSomething(), 5000 );
+
+  // function doSomething ( )
+  // {
+  //     {this.setState.backgroundColor = this.randomHexColor()}, 500 )
+  // }
+
+  clickHandler = (evt) => {
+    this.setState({
+      style: {
+        height: "100px",
+        width: "100px",
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
+
+  mouseOver() {
+    this.setState({
+      style: {
+        height: "150px",
+        width: "150px",
+        // transform: "rotate(90deg)",
+        transition: "all 1s ease-in-out",
+        // borderRadius: "50%",
+        backgroundColor: this.randomHexColor()
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <div style={this.state.style}
+        onMouseOver={this.mouseOver}
+        onClick={this.clickHandler}
+      >
+
+      </div>
+    )
+  }
+
 }
 
-export default App
+export default Pixel
